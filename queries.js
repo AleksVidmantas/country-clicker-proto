67c0c4d049c22cfdf5ed51d330caf0dfeed32cfc
dev <bobopirate1010@gmail.com>
2019-09-04 00:17:52 +0000

    Add actual API files.

---
+++ queries.js
@@ -0,0 +1,24 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  	user: 'postgres',
+ 	host: 'localhost',
+  	database: 'country_game',
+	password: 'aem6443',
+  	port: 5432,
+});
+
+const test = (request, response) => {
+	pool.query('SELECT * FROM faction;', (error, results) => {
+    	if (error) {
+      		throw error
+    	}
+    	response.status(200).json(results.rows)
+	});
+
+}
+
+
+
+module.exports = {
+  test
+}
