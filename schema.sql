05a392485571cc2dc5b90ee689a5975821f88209
Christopher Medlin <christopherjmedlin@gmail.com>
2019-09-07 09:36:25 -0700

    Add a user table to schema.sql
    Since we don't have any sort of database migration system set up, just run the schema.sql again and it should work.

--- schema.sql
+++ schema.sql
@@ -10,6 +10,10 @@ CREATE TABLE regions (id uuid DEFAULT uuid_generate_v4(),
 CREATE TABLE faction (id uuid DEFAULT uuid_generate_v4(), 
                       name VARCHAR(20));
 
+CREATE TABLE users (id uuid DEFAULT uuid_generate_v4(),
+                   username VARCHAR(30),
+                   password VARCHAR(100));
+
 CREATE TABLE user_performance (id uuid DEFAULT uuid_generate_v4(), 
                                clicks_attk INT DEFAULT 0, 
                                clicks_dfnd INT DEFAULT 0);
