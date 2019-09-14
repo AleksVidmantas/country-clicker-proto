7b8609a566fb206761bef5a459c4e77f5aa09de9
IvanKenevich <roranmolotoboyez@gmail.com>
2019-09-13 19:06:37 -0700

    Changed faction table and added region ownership table

--- schema.sql
+++ schema.sql
@@ -6,14 +6,17 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE regions (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY, 
                       click_count INT DEFAULT 0, 
-                      is_parent BOOL DEFAULT FALSE);
+                      name VARCHAR(20));
 
 CREATE TABLE faction (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY, 
                       name VARCHAR(20));
 
+CREATE TABLE region_ownership (region_id uuid,
+                               faction_id uuid);
+
 CREATE TABLE users (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-                   username VARCHAR(30) UNIQUE,
-                   password VARCHAR(100));
+                    username VARCHAR(30) UNIQUE,
+                    password VARCHAR(100));
 
 CREATE TABLE user_performance (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY, 
                                clicks_attk INT DEFAULT 0, 
--- seed.sql
+++ seed.sql
@@ -1,23 +1,33 @@
 \c country_game;
 
-INSERT INTO faction (name) VALUES 
- ('USA')
-,('Russa')
-,('China')
-,('Canada')
-,('Mexico')
-,('North Korea')
-,('Great Britain')
+INSERT INTO faction (id, name) VALUES 
+ ('10000000-a234-4729-bd2a-68379df315fb', 'USA')
+,('20000000-a234-4729-bd2a-68379df315fb', 'Russa')
+,('30000000-a234-4729-bd2a-68379df315fb', 'China')
+,('40000000-a234-4729-bd2a-68379df315fb', 'Canada')
+,('50000000-a234-4729-bd2a-68379df315fb', 'Mexico')
+,('60000000-a234-4729-bd2a-68379df315fb', 'North Korea')
+,('70000000-a234-4729-bd2a-68379df315fb', 'Great Britain')
 ;
 
-INSERT INTO regions (click_count, is_parent) VALUES
- (1000000, true)
-,(1488, false)
-,(99999, false)
-,(123456, false)
-,(8888888, false)
-,(-2, false)
-,(3, true)
+INSERT INTO regions (id, click_count, name) VALUES
+ ('10000000-a234-4729-bd2a-68379df315fb', 1000000, 'Oregon')        -- USA
+,('20000000-a234-4729-bd2a-68379df315fb', 1488, 'California')       -- USA
+,('30000000-a234-4729-bd2a-68379df315fb', 99999, 'Moscow')          -- RUSSIA
+,('40000000-a234-4729-bd2a-68379df315fb', 123456, 'Coahuila')       -- MEXICO
+,('50000000-a234-4729-bd2a-68379df315fb', 8888888, 'Devonshire')    -- GB
+,('60000000-a234-4729-bd2a-68379df315fb', -2, 'Pyongyang')          -- NC
+,('70000000-a234-4729-bd2a-68379df315fb', 3, 'Manchuria')           -- CHINA
+;
+
+INSERT INTO region_ownership (region_id, faction_id) VALUES
+ ('10000000-a234-4729-bd2a-68379df315fb', '10000000-a234-4729-bd2a-68379df315fb')
+,('20000000-a234-4729-bd2a-68379df315fb', '10000000-a234-4729-bd2a-68379df315fb')
+,('30000000-a234-4729-bd2a-68379df315fb', '20000000-a234-4729-bd2a-68379df315fb')
+,('40000000-a234-4729-bd2a-68379df315fb', '50000000-a234-4729-bd2a-68379df315fb')
+,('50000000-a234-4729-bd2a-68379df315fb', '70000000-a234-4729-bd2a-68379df315fb')
+,('60000000-a234-4729-bd2a-68379df315fb', '60000000-a234-4729-bd2a-68379df315fb')
+,('70000000-a234-4729-bd2a-68379df315fb', '30000000-a234-4729-bd2a-68379df315fb')
 ;
 
 INSERT INTO users (username, password) VALUES
